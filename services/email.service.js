c7d84447e65d078852aeca49170e42d57e4664a6
MAyankprine20001 <61388267+MAyankprine20001@users.noreply.github.com>
2025-09-26 22:54:21 +0530

    Update email.service.js

--- services/email.service.js
+++ services/email.service.js
@@ -2,7 +2,12 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
-  auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
+  port: 587,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  connectionTimeout: 10000
 });
 
 module.exports = transporter;
